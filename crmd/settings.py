6c469dad85356eb9400c6472b3800cbcfa60a848
Yameen Ajani <yameenajani@gmail.com>
2020-10-17 17:19:13 +0530

    Changed settings

--- crmd/settings.py
+++ crmd/settings.py
@@ -126,13 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
